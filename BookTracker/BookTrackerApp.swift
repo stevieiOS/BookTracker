89d01cec518618b013ce2005c13e004cdce96062
Steven <v4@outlook.com>
2024-01-20 09:37:15 -0600

    Initial Commit

---
+++ BookTracker/BookTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookTrackerApp.swift
+//  BookTracker
+//
+//  Created by Stevie on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct BookTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
